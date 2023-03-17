9c67d5dde1de242efdbdc984f046359cb146547d
laila <lailatabourit@gnail.com>
2023-03-16 19:28:51 -0700

    task 4-5-6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-      puts("\"Programming is like building a multilingual puzzle");
-      return (0);
-}      
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}

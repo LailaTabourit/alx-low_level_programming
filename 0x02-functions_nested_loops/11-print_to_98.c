942738d7ef696d6af30b1bba152c1e4dfd248e09
laila <lailatabourit@gnail.com>
2023-03-21 19:49:26 -0700

    ALX_tasks

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,20 +7,14 @@
 */
 void print_to_98(int n)
 {
-	int i;
 
-	if (n < 98)
+	while (n != 98)
 	{
-		for (i = n; i <= 98; i++)
-		{
-			printf("%d, ", i);
-		}
+		printf("%d, ", n);
+		if (n > 98)
+			n--;
+		else
+			n++;
 	}
-	else if (n > 98)
-	{
-		for (i = n; i >= 98; i--)
-		{
-			 printf("%d, ", i);
-		}
-	}
+	printf("%d\n", n);
 }

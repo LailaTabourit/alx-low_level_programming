1facd4b03cb5341fa612bb5e1b66d5ce59564fc9
laila <lailatabourit@gnail.com>
2023-03-21 17:57:43 -0700

    ALX_tasks

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
-*int print_sign - prints the sign of a number.
-*@n:The character to be checked.
-*
-*/
+ * print_sign - Prints the sign of a number.
+ * @n: The number of which the sign will be printed.
+ *
+ * Return: 1 if the number is greater than zero,
+ *         0 if the number is zero,
+ *         -1 if the number is less than zero.
+ */
 int print_sign(int n)
 {
 	if (n > 0)

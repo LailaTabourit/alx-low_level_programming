e951df735b03622e137eb49bed7b83f2798b3a56
laila <lailatabourit@gnail.com>
2023-03-21 17:55:54 -0700

    ALX_tasks

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*int print_sign - prints the sign of a number.
+*@n:The character to be checked.
+*
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}

83f199cf060d99ee50ca80a173beb85ae41fb092
laila <lailatabourit@gnail.com>
2023-03-23 09:19:23 -0700

    ALX_tasks

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	positive_or_negative(n);
+
+	return (0);
+}

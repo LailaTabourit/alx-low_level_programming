590a7e007b4e5f4ff264c6dbe1d7f8c116f8f8b4
laila <lailatabourit@gnail.com>
2023-03-27 17:07:47 -0700

    ALX_tasks

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -  a function that take pointer and update value to 98
+ * @n: pointer to the variable
+ * Return: void return nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

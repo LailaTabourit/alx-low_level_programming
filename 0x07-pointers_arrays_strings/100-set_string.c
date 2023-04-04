76827e986801ca88d771c2a3e2125d20fab8a864
laila <lailatabourit@gnail.com>
2023-04-03 18:20:23 -0700

    ALX_tasks

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* set_string - a function that set string to a char
+* @s: pointer variable
+* @to: pointer variable
+* Return: void return nothing
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

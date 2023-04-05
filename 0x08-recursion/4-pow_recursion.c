62d3427c3c6e2c72c455dbb2b68c39ac1c47083d
laila <lailatabourit@gnail.com>
2023-04-05 10:05:17 -0700

    ALX_tasks

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_pow_recursion - a function that gets x power y
+* @x: value of raise
+* @y: value of power
+*
+* Return: result of the power
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	if (y < 0)
+		return (-1);
+
+	return (x * _pow_recursion(x, y-1));
+}

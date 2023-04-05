f76ca3aa6cd1cb9d6293533978e1a77fa79a34d2
laila <lailatabourit@gnail.com>
2023-04-05 09:53:27 -0700

    ALX_tasks

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* factorial - a function that return factor of number
+* @n: variable input
+* Return: Factorial of n
+*/
+
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+	return (n * factorial(n - 1));
+}

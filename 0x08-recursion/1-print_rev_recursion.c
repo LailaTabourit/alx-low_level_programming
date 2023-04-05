fbf1aab01932581fefeefd805a7984af5730150e
laila <lailatabourit@gnail.com>
2023-04-05 09:25:03 -0700

    ALX_tasks

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
-* _
-print_rev_recursion - a function print string in reverse
+* _print_rev_recursion - a function print string in reverse
 * @s: pointer variable
 * Return: void return nothing
 */

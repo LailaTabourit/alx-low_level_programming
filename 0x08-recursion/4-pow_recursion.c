b34be57e04460f9913595e2251a1d4eef96474a2
laila <lailatabourit@gnail.com>
2023-04-05 13:13:02 -0700

    ALX_tasks

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,15 +5,17 @@
 * @x: value of raise
 * @y: value of power
 *
+* by: laila tabourit
 * Return: result of the power
 */
 
 int _pow_recursion(int x, int y)
 {
+
 	if (y == 0)
 		return (1);
 	if (y < 0)
 		return (-1);
 
-	return (x * _pow_recursion(x, y-1));
+	return (x * _pow_recursion(x, y - 1));
 }

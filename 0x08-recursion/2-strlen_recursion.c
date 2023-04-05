7b19f7abd882f18abc4a429461e524c42ea02586
laila <lailatabourit@gnail.com>
2023-04-05 09:46:39 -0700

    ALX_tasks

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _strlen_recursion - a function that return length of string
+* @s: pointer variable
+* Return: length of string
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	return (1 + _strlen_recursion(s + 1));
+}

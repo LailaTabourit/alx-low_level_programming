007bbc31624834e800ccf22696a3ea4154b0b9c9
laila <lailatabourit@gnail.com>
2023-04-05 10:48:12 -0700

    ALX_tasks

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int sqrt_recursion(int n, int i);
+
+/**
+* _sqrt_recursion - return natural square of number
+* @n: number
+*
+* Return: the result square
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (sqrt_recursion(n, 0));
+}
+
+/**
+* sqrt_recursion - a function that find the natural square root of a number
+* @n: number to calc the square root of i
+* @i: number of iteration
+*
+* Return: the resulting square root
+*/
+
+int sqrt_recursion(int n, int i)
+{
+	if(i * i == n)
+		return (i);
+	return (sqrt_recursion(n, i + 1));
+}

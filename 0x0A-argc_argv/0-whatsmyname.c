67ba280804e1e11ba6eeef6bc9f788da76bacd35
laila <lailatabourit@gnail.com>
2023-04-09 19:23:59 -0700

    ALX_tasks

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - If you rename the program, it will print the new name
+*
+* @arg: variable
+* @argv: variable
+* by : laila tabourit
+*
+* Return: always 0
+**/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+
+	printf("%s", argv[0]);
+	return (0);
+}

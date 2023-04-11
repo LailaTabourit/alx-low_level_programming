9abf506b55cd2960b188351525b22f1d9270cceb
laila <lailatabourit@gnail.com>
2023-04-10 17:03:59 -0700

    ALX_tasks

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+* main - print the result of the multiplication
+*
+* @argc: variable
+* @argv: variable
+*
+* by: laila tabourit
+* Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int a = atoi(argv[1]);
+	int b = atoi(argv[2]);
+	int result;
+
+	result = a * b;
+	if (argc == 3)
+	{
+		printf("%d\n", result);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}

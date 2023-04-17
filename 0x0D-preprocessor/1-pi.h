64fc7780456279e51bb29ab7284595b99129c76e
laila <lailatabourit@gnail.com>
2023-04-17 15:05:59 -0700

    ALX_tasks

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+#ifndef PI_H
+#define PI_H
+
+#define PI 3.121259265359
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - print the name of the file
+*
+* by: lailitaa
+* Return: always 0
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

50fb9926ab13c0201915a93805c4816a466d8f70
laila <lailatabourit@gnail.com>
2023-04-17 06:22:51 -0700

    ALX_tasks

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* struct dog - structure
+* @name: first member
+* @owner: second member
+* @age: third member
+*
+* Description: structure
+*/
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif

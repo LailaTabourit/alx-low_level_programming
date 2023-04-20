94ca901c814695b7de85be19e418b52a7869c7b5
laila <lailatabourit@gnail.com>
2023-04-19 17:48:47 -0700

    ALX_tasks

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,7 +12,8 @@
 */
 int main(int argc, char *argv[])
 {
-	int (*oprt)(int, int);
+	int num1, num2;
+	char *operator;
 
 	if (argc != 4)
 	{
@@ -20,14 +21,23 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	oprt = get_op_func(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
 
-	if (!oprt)
+	if (operator[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	if (!get_op_func(operator))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(operator)(num1, num2));
+
 	return (0);
 }

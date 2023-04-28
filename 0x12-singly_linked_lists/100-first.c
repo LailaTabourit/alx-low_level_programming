045079048dd012a1c8b328b244e5bcb8b7188d71
laila <lailatabourit@gmail.com>
2023-04-27 17:24:33 -0700

    ALX_tasks

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+* first - function print
+* by : lailita
+*
+* Return: void
+*/
+void first(void) __attributr__((constructor));
+
+void first(void)
+{
+	printf("You're beast! and yet, you must allow, \n");
+	printf("I bore my house upon my back!\n");
+}
+

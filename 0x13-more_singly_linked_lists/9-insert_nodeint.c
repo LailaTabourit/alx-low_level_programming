6a53393d3b4996003d980d8c23c34095e590ce37
laila <lailatabourit@gmail.com>
2023-05-01 10:53:10 -0700

    ALX_tasks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,9 +9,10 @@
 * Return: address of new node
 */
 
-listint_t *insert_node_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *tmp = *head;
+	listint_t *new;
+	listint_t *tmp = *head;
 	unsigned int i;
 
 	new = malloc(sizeof(listint_t));

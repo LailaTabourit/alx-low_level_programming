a7ddb0447a68a8013ad99763a26ec73131f8d4ac
laila <lailatabourit@gmail.com>
2023-05-01 09:43:03 -0700

    ALX_tasks

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+* print_listint_safe - prints a list
+* @head: pointer
+*
+* by: lailitaa
+* Return: the number of nodes
+*/
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t i = 0;
+	long int dif;
+
+	while (head)
+	{
+		dif = head - head ->next;
+		i++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (dif > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
+	}
+	return (i);
+}
